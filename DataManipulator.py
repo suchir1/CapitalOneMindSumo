faf341c445f3ba722f643171b6817db2fe649748
Suchir <suchir.bhatt@gmail.com>
2018-03-13 21:36:40 -0400

    Initial commit

--- DataManipulator.py
+++ DataManipulator.py
@@ -5,3 +5,9 @@ cwd = os.getcwd()
 print(cwd)
 dataSet = pd.read_csv(cwd + "/Data/sfpd_dispatch_schema.csv")
 print(dataSet)
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
