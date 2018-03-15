19bd50f17f51e7f4657b1008dae55b99c1b901bd
Suchir <suchir.bhatt@gmail.com>
2018-03-14 23:58:29 -0400

    Delay times calculated and stored and call frequency heatmap created

--- HeatmapCreator.py
+++ HeatmapCreator.py
@@ -3,7 +3,6 @@ from gmplot import gmplot
 import os
 import pandas as pd
 
-
 cwd = os.getcwd()
 dataSet = pd.read_csv(cwd + "/Data/sfpd_dispatch_data_subset.csv")
 gmapFrequency = gmplot.GoogleMapPlotter(37.762289, -122.445359, 13)
